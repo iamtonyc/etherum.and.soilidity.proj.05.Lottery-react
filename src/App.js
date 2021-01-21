322018177b8a7194d0af57572d66e568fe1d2fe5
Tony Cheung <iamtonycheung@gmail.com>
2021-01-22 01:42:43 +0800

    update to lesson 96

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import web3 from './web3';
-import lottery from './lottery';
-import { Component } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import web3 from "./web3";
+import lottery from "./lottery";
+import { Component } from "react";
 
-class App extends Component{
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props);
-    this.state ={manager: ''};
+    this.state = { manager: "" };
   }
-  async componentDidMount(){
-    const manager=await lottery.methods.manager().call();
-    this.setState({manager});
-
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    this.setState({ manager });
   }
 
-   
-
-  render(){
+  render() {
     return (
-    <div>
+      <div>
         <h2>Lottery Contract</h2>
         <p>This contract is managed by {this.state.manager}</p>
-    </div>
-  );
+      </div>
+    );
   }
 }
 
